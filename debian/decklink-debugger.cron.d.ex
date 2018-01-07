bfb31e12050d3b4a4d4d4d17037902e1e6990c7d
MaZderMind <git@mazdermind.de>
2018-01-07 19:01:11 +0100

    modify debian config

--- debian/decklink-debugger.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the decklink-debugger package
-#
-0 4	* * *	root	[ -x /usr/bin/decklink-debugger_maintenance ] && /usr/bin/decklink-debugger_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(decklink-debugger):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="decklink-debugger" command="/usr/bin/decklink-debugger"
