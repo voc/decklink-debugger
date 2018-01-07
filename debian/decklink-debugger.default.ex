3ab7d41f2d39727a3798b4896a280ded5d808b83
MaZderMind <git@mazdermind.de>
2018-01-07 18:57:24 +0100

    initial debian config

---
+++ debian/decklink-debugger.default.ex
@@ -0,0 +1,10 @@
+# Defaults for decklink-debugger initscript
+# sourced by /etc/init.d/decklink-debugger
+# installed at /etc/default/decklink-debugger by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(decklink-debugger):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="decklink-debugger" command="/usr/bin/decklink-debugger"
