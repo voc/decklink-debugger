voc/decklink-debugger

commit ce45c1b5a4fcedcc55db2a962c2b21dfd605fc4e

fix query for persistent id of iter-device

CaptureDelegate.cpp
@@ -141,7 +141,7 @@ IDeckLink* CaptureDelegate::queryDeckLinkInterfaceByPersistentId(int64_t pairedD
 		}
 
 		int64_t persistent_id;
-		if (attr->GetInt(BMDDeckLinkPersistentID, &persistent_id) == S_OK) {
+		if (attr->GetInt(BMDDeckLinkPersistentID, &persistent_id) != S_OK) {
 			attr->Release();
 			iter->Release();
 			return NULL;
@@ -173,7 +173,7 @@ void CaptureDelegate::setDuplexToHalfDuplexModeIfSupported(void)
 		// failed. trying paired device
 
 		IDeckLink *pairedDecklink = queryDeckLinkInterfaceByPersistentId(m_pairedDeviceId);
-		if(!pairedDecklink)
+		if(pairedDecklink == NULL)
 		{
 			// no paired device, nothing left to do.
 			return;

commit 397b1669ed676d2158e069a9e89d2228bbd4f411

partially revert ce45c1b5a4fcedcc55db2a962c2b21dfd605fc4e which broke duo2 support

CaptureDelegate.cpp
@@ -140,7 +140,7 @@ IDeckLink* CaptureDelegate::queryDeckLinkInterfaceByPersistentId(int64_t pairedD
 		}
 
 		int64_t persistent_id;
-		if (attr->GetInt(BMDDeckLinkPersistentID, &persistent_id) != S_OK) {
+		if (attr->GetInt(BMDDeckLinkPersistentID, &persistent_id) == S_OK) {
 			attr->Release();
 			iter->Release();
 			return NULL;